fd017f86f7c4f12ea87355937ec41662f572e668
Alex Greenwood <agreenwood43@googlemail.com>
2022-12-13 11:11:29 +0000

    added new search method to script.js and added logging to worker

--- script.js
+++ script.js
@@ -22,8 +22,6 @@ function setup() {
 }
 
 //#region fetching images
-
-var carefulSearch=false;
 var idLen = 5;
 
 async function getNewImage() {    
@@ -43,21 +41,40 @@ async function getNewImage() {
             var newWorker = new Worker("worker.js")
             newWorker.addEventListener("message", function(msg) {
                 var data = msg.data
+
+                switch (true) {
+                    case data.startsWith("@"): 
+                        idLabel.innerHTML = "ID: " + data.replace("@", "")
+                        break;
+                    case data.startsWith("!"):
+                        msg = data.replace("!", "")
+                        showErrorToUser(msg)
+                        pool.forEach((worker) => {
+                            worker.terminate()
+                        })
+                        break;
+                    case data.startsWith("#"):
+                        msg = data.replace("#", "")
+                        console.log(msg)
+                        break;
+                    default: 
+                        pool.forEach((worker) => {
+                            worker.terminate()
+                        })
+        
+                        disableControls(false)
+                        pushImage(data)
+                }
                 if (data.startsWith("@")) {
-                    idLabel.innerHTML = "ID: " + data.replace("@", "")
+                    
                 } else {
-                    pool.forEach((worker) => {
-                        worker.terminate()
-                    })
-    
-                    disableControls(false)
-                    pushImage(data)
+                    
                 }                
             })
             pool.push(newWorker)
         }
         pool.forEach((worker) => {
-            worker.postMessage(idLen.toString() + "," + carefulSearch.toString())
+            worker.postMessage(idLen.toString())
         })        
     }    
 }
@@ -116,44 +133,25 @@ function getUrl(id, asThumbnail) {
 //#endregion
 
 //#region testing images
-const errorImgSize = [161, 81]
-const errorImgB64 = "iVBORw0KGgoAAAANSUhEUgAAAKEAAABRCAYAAAC6w8avAAAGF0lEQVR4Xu2cYa7cIAyE26u8d/8jvbO0SiUkF9meMZCwSaZ/2m7A2OMPQ8hmf399ff35pT9SYKMCvwXhRvU19D8FBKFA2K6AINyeAjkgCMXAdgUE4fYUyAFBKAa2K5BC+PPzEzr4/f3967h+/D37Z5WdWT/Uf48CdCX0QBE8e5L2tFGXQNgqplcVbTWNqmYPc/t/bzcaJxvD9okmUktq5L/93PPV6x/FFAEUxWC1aH4wmt4J1GkIj2CtOChhbKJ7u9E4WbLtNQ9gBhTW/uFfP16mhYUEjVHR+E7wNV+nIaxAFy3fCAZ0PUoo6oeuN7teNeqhm4GwB6cC8xO2RKdD2AvMVsIq3HacSmWu+IegrcDjgcfGwPh8p4p4OoTM3XMluZWKU7Wb7de80wC0jK5ajlk7dwLP+nophGcsx9m+b8WesF+SK3s5Fh42BrQFEITdxtwmr/27enccARDdoUbVqm3sR+6OMwgbFFF89i7W3lx4sNgbp2xZR2PeEUS6Et4xuGzDX43nCTcA1Zivav9oCCuVKBNcAJ6L46MhPFc6WV+lgCBcpaTsDCsgCIelU8dVCjwCQnQ3uUqs7HjmjDFW2Vy5p63YYts+DsJViUN2WIGRnSuu7/KVHRdC6D03tXed2Tdn+nM75glGdOYWnY/1vkRj2HO4vqK1MzwkWnSWh87uWL2aHx6YNg9RO28cNHamq3fYzp61MuO2OCkIbdAMSP1XjphnuRW7TTjPbnaQjfoxENiknR1XNFn6OND/s8mV6R4VIPa5PprUJQjZQatiZHs5BGW0P0P9KmNme0DWTsWfbGmu2sn8q8bFxtr7fyqE/WAspGjWZXazJa8iUqVtNVmtfT/GaFzs+NXJ71VY+xmbTxtv+zf7vNyORy3HFcMItMgWO2uiRDNLY7Yce4lkIajEnFW7zIerKqEXM1tVkb5R7NMQogQcAzOArBK5YserWCNfskAaVCpLNBkrcVVgYHyvrB4VYIf3hNmy2Jfn7FstUfn2PmfKfoN9JFlR337mHrazZSvy3fbLThPshPXGRitSNE77PJtgve+Rjt4kGR2XhhAtH+h6dZlF9s68fidfz9ThattwOZ516JMTm1W22bjVn1fgdAh5V9TyrQoIwrdm/oPiFoQflIy3uiII35r5D4r7VAh335TsHv/qPKPjqcgfpBO6PhvnqRDOOjfb/2zxZv07uz8bP2qHrs/GQUGYHUZ6B9LNKW9m9oeiXgCtX3TIig5/R8Znx0QH8P2BsLVbOahGTzI8HVElZPJ42I1isLoyeWThhBAyYjCPupBA1uEePtu3aod5ZHiMzY6ZtWt2+icbXmKjtshflA/22XyvqfVxdAwWur7dNIQRgL3IlZK+SoQKvKNjokmxIu7MRiXGfqIj4L0VBcU7AuJlENpqE1WGbBllBVshNJpAXhWx46JnvF7Vz5bXbFvDVrBe/2o/q38PWlaIGCgvhTACpHd0tCpldtAMHh0TVTq2imXLM1vtVsVQscNAhtpcBiGCIAOUTcIVEI7sCdk9s7dCICDYiob2gCN2vImDgPOuQwizZbRaBVoCZ5bj2WXdW/ayKmS3CM3vbHlEd5beRInsRkm2E6EyQVG/foLZ2L2brT6PiIcIUArCEbqf3GdU7CdrMhObICTUsxUcVXHCnJp0CghCIbFdAUG4PQVyQBCKge0KCMLtKZADglAMbFdAEG5PgRwQhGJguwKCcHsK5IAgFAPbFRCE21MgBwShGNiugCDcngI5IAjFwHYFBOH2FMgBQSgGtisgCLenQA4IQjGwXQEIob7Kvj1Hj3cAQvh4BRTgdgUghOjNMu8NLe+djOjVxewNvOga+3n09th21eXAfwoMQej9GkKDLIPN+2WCaLmv2jmiil5/1Jbis6m/DYRNRvQyeDZBPjsV7/XuNhCiSqtKeF+Il0PYYLCSRD+mE/3Wn9e++gsA2S8T3Dddz/QcQlgNO1suq7bU/h0KLIewr4SzPxv2jjS8O8pTIHy3pIq+qoAgrCqm9ssVEITLJZXBqgKCsKqY2i9XQBAul1QGqwoIwqpiar9cAUG4XFIZrCogCKuKqf1yBf4C8TXYv71GQx0AAAAASUVORK5CYII=";
 const monopolyManSize = [298, 256]
 const mcSkinSize = [64, 32]
 
 function testUrl(url) {
 
     // Define the promise
-    let imgPromise = new Promise(function imgPromise(resolve, reject) {
+    let imgPromise = new Promise(async function imgPromise(resolve, reject) {
 
-        // Create the image
-        let imgElement = new Image();
-        imgElement.crossOrigin = "anonymous";
+        var img = await fetch(url)
 
-        // When image is loaded, resolve the promise
-        imgElement.addEventListener('load', function imgOnLoad() {
-            if(imgElement.width != errorImgSize[0] && imgElement.height != errorImgSize[1]) {
-                resolve(this);
-            } else if (carefulSearch) {
-                var b64 = getBase64Image(imgElement)
-                if (b64 != errorImgB64) {
-                    resolve(this)
-                } else {
-                    reject()
-                }
+        if (img.status == 200) {
+            if (img.url !== "https://i.imgur.com/removed.png") {
+                resolve()
             } else {
                 reject()
-            }           
-        });
-
-        // When there's an error during load, reject the promise
-        imgElement.addEventListener('error', function imgOnError() {
-            window.alert("Imgur is unreachable")
-            throw new Error("Imgur is unreachable");
-        })
-
-        // Assign URL
-        imgElement.src = url;
+            }
+        } else {
+            showErrorToUser("imgur is unreachable")
+        }
 
     });
 
@@ -183,25 +181,6 @@ function checkForFunny(img) {
             title.removeAttribute("style")
             title.innerHTML = "Endless∞Imgur"
     }
-}
-
-function getImgData(img) {
-    var canvas = document.createElement("canvas");
-    canvas.width = img.width;
-    canvas.height = img.height;
-    var ctx = canvas.getContext("2d");
-    ctx.drawImage(img, 0, 0);
-    return ctx.getImageData(0, 0, img.width, img.height);
-}
-
-function getBase64Image(img) {
-    var canvas = document.createElement("canvas");
-    canvas.width = img.width;
-    canvas.height = img.height;
-    var ctx = canvas.getContext("2d");
-    ctx.drawImage(img, 0, 0);
-    var dataURL = canvas.toDataURL("image/png");
-    return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
 }
 
 //#endregion
@@ -289,15 +268,6 @@ function setThreadCount(num) {
     if (num % 1 == 0){
         threads = num
     }    
-}
-
-function toggleCarefulSearch() {
-    if (document.getElementById("carefulSearchToggle").checked) {
-        carefulSearch = true
-    } else {
-        carefulSearch = false
-    }
-
 }
 
 function toggleIdLen() {
@@ -408,6 +378,11 @@ function arraysIdentical(a, b) {
     }
     return true;
 };
+
+function showErrorToUser(msg) {
+    window.alert(msg);
+    throw new Error(msg);
+}
 
 //#endregion
 
